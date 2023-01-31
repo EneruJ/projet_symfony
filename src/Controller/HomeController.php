3762c42879b1defa25f5e99a5fad04ae9d19a7d4
TPLulu <thibault.piotrowski@gmail.com>
2023-01-31 14:38:56 +0100

    interface

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,15 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Journee;
+use App\Entity\User;
 use Doctrine\Persistence\ManagerRegistry;
 class HomeController extends AbstractController
 {
     public function index(ManagerRegistry $doctrine): Response
     {
         $list = $doctrine->getRepository(Journee::class)->findAll();
+        $user = $this->getUser();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'list' => $list,
+            'user' => $user,
         ]);
     }
 
