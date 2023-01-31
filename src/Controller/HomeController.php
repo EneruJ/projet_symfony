fcef0394f4200359bdc74157755554b7b8cde774
TPLulu <thibault.piotrowski@gmail.com>
2023-01-31 11:53:23 +0100

    list journée

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,13 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Entity\Journee;
+use Doctrine\Persistence\ManagerRegistry;
 class HomeController extends AbstractController
 {
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $list = $doctrine->getRepository(Journee::class)->findAll();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'list' => $list,
         ]);
     }
+
 }
