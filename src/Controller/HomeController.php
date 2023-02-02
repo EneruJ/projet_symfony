332699d60271b8b8e3cd07574236c97c5998b0f1
TPLulu <thibault.piotrowski@gmail.com>
2023-02-02 10:28:15 +0100

    menu + page mes participations

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -43,4 +43,36 @@ class HomeController extends AbstractController
         ]);
     }
 
+    public function mesjours(ManagerRegistry $doctrine): Response
+    {
+        $list = $doctrine->getRepository(Journee::class)->findAll();
+        $user = $this->getUser();
+        $in_list = array();
+        if ($user) {
+            $user_id = $user->getId();
+            
+            foreach ($list as $key => $value) {
+                $this_id = $list[$key]->getId();
+                $is_in = $doctrine->getRepository(UserJournee::class)->findBy(array('journee' => $list[$key]));
+                if ($is_in) {
+                    $ok = 0;
+                    foreach ($is_in as $key2 => $value2) {
+                        if ($is_in[$key2]->getUser() == $user) { 
+                            $ok = 1;
+                        }
+                    }
+                    $in_list[$this_id] = $ok;
+                }
+                
+            }
+        }
+        
+        return $this->render('journee/mesjours.twig', [
+            'controller_name' => 'HomeController',
+            'list' => $list,
+            'user' => $user,
+            'in_list' => $in_list,
+        ]);
+    }
+
 }
